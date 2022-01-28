8310b061eefaf9cb5ea2f2b5660a01e2fd4f0b2c
shreyaj2000 <shreya.sharad.jadhav@gmail.com>
2022-01-28 18:46:43 +0900

    Logging more memory and cpu info

--- parameterExtract.py
+++ parameterExtract.py
@@ -14,12 +14,29 @@ def main():
     print(os.popen('uptime -p').read()[:-1])
     # Sensor temperatures
     print(psutil.sensors_temperatures())
-    # Total physical memory in gb
-    print('total physical memory:' + str(psutil.virtual_memory().total/10**9)+ 'GB')
-    # Memory currently in use, and so it is in RAM
-    print('RAM usage:', str(psutil.virtual_memory().active/10**6)+ 'MB')
-    # gives a single float value
+    
+    '''
+    CPU Information
+    '''
+    # CPU utilization as a percentage
     print('cpu %:' + str(psutil.cpu_percent()))
+    # Number of logical CPUs in the system
+    print('cpu count:' + str(psutil.cpu_count()))
+    # CPU statistics (monitoring system call  for malware attack)
+    print(psutil.cpu_stats())
+    # CPU Frequency/clock speed
+    print('cpu freq:' + str(psutil.cpu_freq()))
+    # average system load over the last 1, 5 and 15 minutes
+    # Processes which are using the CPU or waiting to use the CPU
+    print('system load:' + str([x / psutil.cpu_count() * 100 for x in psutil.getloadavg()]))
+    
+    '''
+    Memory Information
+    '''
+    # Total physical memory in gb
+    print('total physical memory:' + str(psutil.virtual_memory().total/10**9)+ ' GB')
+    # Memory currently in use, and so it is in RAM
+    print('RAM usage:', str(psutil.virtual_memory().active/10**6)+ ' MB')
     # you can calculate percentage of available memory
     print('memory % available:' + str(psutil.virtual_memory().available * 100 / psutil.virtual_memory().total))
     # you can have the percentage of used RAM
