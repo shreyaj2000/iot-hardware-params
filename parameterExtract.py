a42e55529191c7024d2534c3b2a08a1e70f70716
shreyaj2000 <shreya.sharad.jadhav@gmail.com>
2022-01-29 01:05:02 +0900

    Add network and boot parameters

--- parameterExtract.py
+++ parameterExtract.py
@@ -2,6 +2,7 @@
 import psutil
 import tracemalloc
 import os
+import datetime
  
 
 def main():
@@ -14,11 +15,13 @@ def main():
     print(os.popen('uptime -p').read()[:-1])
     # Sensor temperatures
     print(psutil.sensors_temperatures())
+    # Boot Time
+    print('boot time: '+ str(datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("%Y-%m-%d %H:%M:%S")))
     
     '''
     CPU Information
     '''
-    # CPU utilization as a percentage
+    # CPU utilization as a percentage. percentage of processor being used
     print('cpu %:' + str(psutil.cpu_percent()))
     # Number of logical CPUs in the system
     print('cpu count:' + str(psutil.cpu_count()))
@@ -40,7 +43,29 @@ def main():
     # you can calculate percentage of available memory
     print('memory % available:' + str(psutil.virtual_memory().available * 100 / psutil.virtual_memory().total))
     # you can have the percentage of used RAM
-    print('memory % used:', str(psutil.virtual_memory()[2]))
+    print('memory % used:', str(psutil.virtual_memory().percent))
+    
+    '''
+    Disk Usage
+    '''
+    # hard drive storage. How much percentage of storage (HDD, SSD) being used.
+    print(psutil.disk_usage('/'))
+    
+    '''
+    Network Peformance
+    '''
+    # Bytes sent
+    print('number of bytes sent:' + str(psutil.net_io_counters().bytes_sent))
+    # Bytes received
+    print('number of bytes received:' + str(psutil.net_io_counters().bytes_recv))
+    # Packets sent
+    print('number of bytes sent:' + str(psutil.net_io_counters().packets_sent))
+    # Packets received
+    print('number of bytes sent:' + str(psutil.net_io_counters().packets_recv))
+    # Info about each Network Interface Card
+    print(psutil.net_if_stats())
+    
+    
 
     print('---------------------------')
     current, peak = tracemalloc.get_traced_memory()
